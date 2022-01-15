350ea49068a6065479756e74a0fb3f0c716bba9c
AyushDabhi <76897206+AyushDabhi@users.noreply.github.com>
2022-01-15 19:40:24 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,26 @@
+# 20CE015 (Ayush Dabhi)
+# A.
+
+tuple1 = (1, 2, 3, 4)
+tuple2 = ("Hello", "World")
+print(tuple1)
+print(tuple2)
+
+# B.
+tuple = (1, 2, 3, 4)
+print(tuple[3])
+
+# C.
+tuple = (1, 2, 3, 4)
+tuple = tuple + (5, 6)
+print(tuple)
+
+# D.
+tuple = ('H', 'e', 'l', 'l', 'o')
+str = ''.join(tuple)
+print(str)
+
+# E.
+tuple = ('H', 'e', 'l', 'l', 'o')
+print("Length of the tuple:", len(tuple))
+
