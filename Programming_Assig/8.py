6c738f28f4fd6ede0da84193866d5bc7d683071a
AyushDabhi <76897206+AyushDabhi@users.noreply.github.com>
2022-04-20 22:01:05 +0530

    Add files via upload

---
+++ Programming_Assig/8.py
@@ -0,0 +1,31 @@
+"""
+ID : 20CE0015
+NAME : Ayush Dabhi
+GITHUB REPOSITORY LINK :
+"""
+
+
+
+class P1_class():
+
+
+def show(self):
+    print("Inside Parent Class 1")
+
+
+class P2_class():
+
+
+def display(self):
+    print("Inside Parent Class 2")
+
+
+class Child_class(P1_class, P2_class):
+
+    def show(self):
+      print("Inside Child Class")
+
+obj = Child_class()
+
+obj.show()
+obj.display()
