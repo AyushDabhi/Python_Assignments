34f18e49284af2f73aa90610dbadff7fe07b9396
AyushDabhi <76897206+AyushDabhi@users.noreply.github.com>
2022-04-20 21:37:22 +0530

    Add files via upload

---
+++ Programming_Assig/2.py
@@ -0,0 +1,18 @@
+"""
+ID : 20CE0015
+NAME : Ayush Dabhi
+GITHUB REPOSITORY LINK :
+"""
+
+import statistics
+
+number = list(map(int,input().split()))
+
+
+max_number = max(number)
+min_number = min(number)
+mean_number = round(statistics.mean(number),2)
+standard_deviation = round(statistics.stdev(number),2)
+variance_number = round(statistics.variance(number),2)
+
+print(max_number, min_number, mean_number, standard_deviation, variance_number)
